d491da7240ff162d2ddb92095fc33b56e795dbb9
Fabio <fabio@ipropertys-imac.iproperty.com.au>
2014-03-13 17:34:51 +1100

    more development

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,29 +3,46 @@ class PropertiesController < ApplicationController
 
     @session_token = Rpdata.generate_token( 'bsguser.iproperty', 'x5yGCV85', '500998' , '43376848' )
     session[:rp_data_token] = @session_token
-    session[:api_key] = '8f894f92ce07e843e5b63756a1a600018d87aa04'
+    session[:api_key] = 'bf3621c935c397a4f94e3adb49afaaa1270f4701'
     
   	if !params[:address].blank? 
   		rp_data_id = Rpdata.id_for_address( @session_token, params[:address] )
     else
-  	 rp_data_id = '6168318'
+  	 rp_data_id = Rpdata.id_for_address(@session_token, '7 Duke Street, Sunshine Beach QLD')
     end
    	
    	@property_details = Rpdata.appraisal_data( @session_token, rp_data_id )
     
-    @property_history = Rpdata.property_history( @session_token, rp_data_id ) #this is still using valuers
+    #@property_history = Rpdata.property_history( @session_token, rp_data_id ) #this is still using valuers
 
     @history_details = Rpdata.property_details( @session_token, rp_data_id )
-    puts "what historory details:"
-    puts @history_details
-
-    @comparable_otms = Rpdata.comparable_otms( @session_token, rp_data_id )
+    #@comparable_otms = Rpdata.comparable_otms_valuers( @session_token, rp_data_id )
+        
+    #new way of getting recent sales and market comparison
+    property = Rpdata.fetch_property(@session_token, rp_data_id)
+    close_property_ids = Rpdata.property_ids_by_nearest_suburb( @session_token, rp_data_id ).collect(&:property_id)
+    #this should return an array of ids.
     
+    recent_sale_ids = Rpdata.refine_sold_properties( @session_token, close_property_ids, property )
+    if recent_sale_ids.count < 5 
+      recent_sale_ids = Rpdata.recent_sales( @session_token, close_properties_ids )
+    end
+
+    @recent_sales = []
+    recent_sale_ids.each do |sale_id|
+      @recent_sales << Rpdata.property_summary( @session_token , sale_id )
+    end
+
+    
+    
+    otm_ids = Rpdata.refine_otm_properties( @session_token, close_property_ids, property )
+    @comparable_otms = Rpdata.otm_property_summary_list( @session_token, otm_ids )
+
     @comparable_otms_avg_price = comparable_otms_avg_price( @comparable_otms )
     @comparable_otms_avg_dom = comparable_otms_avg_dom( @comparable_otms )
-    @market_comparisons = Rpdata.comparable_sales( @session_token, rp_data_id )
+    #end of the new way.
 
-    @market_comparisons_avg_price = comparable_sales_avg_sold( @market_comparisons )
+    @market_comparisons_avg_price = comparable_sales_avg_sold( @recent_sales )
 
     @suburb_stats = Rpdata.suburb_stats(@session_token, @property_details.suburb, @property_details.post_code, @property_details.state )
 
@@ -48,15 +65,15 @@ class PropertiesController < ApplicationController
   private
 
   def comparable_otms_avg_price( comparable_otms )
-    comparable_otms.collect{|x| x.price.to_i}.sum / comparable_otms.count
+    comparable_otms.collect{|x| x[:current_listing_price].to_i}.sum / comparable_otms.count
   end
 
   def comparable_otms_avg_dom( comparable_otms )
-    comparable_otms.collect{ |x| Date.today.mjd - ( Date.parse( x.listed_date.to_s ).mjd )  }.sum / ( comparable_otms.count )
+    comparable_otms.collect{ |x| x[:days_otm].to_i }.sum / ( comparable_otms.count )
   end
 
   def comparable_sales_avg_sold( market_comparisons )
-    market_comparisons.collect{ |x| x.price.to_i }.sum / market_comparisons.count
+    market_comparisons.collect{ |x| x[:property_sale][:transfer_price].to_i }.sum / market_comparisons.count
   end
 
 end
--- app/helpers/rpdata_helper.rb
+++ app/helpers/rpdata_helper.rb
@@ -2,44 +2,44 @@ require 'openssl'
 module RpdataHelper
 
   def sales_cma_link(property_id)
-    hash = {src: "test", inputPropertyId: property_id}
-    build_url do |params|
-      link_to( image_tag("cma.png") + " CMA Report" , "#{base_link_url}flow/cma.html?#{hash.merge(params).to_query}", { target: "_blank"})
+    hash = "src=test&inputPropertyId=#{property_id}"
+    build_url(hash) do |params|
+      link_to( image_tag("cma.png") + " CMA Report" , "#{base_link_url}flow/cma.html?#{params}", { target: "_blank"})
     end
   end
 
   def rental_cma_link(property_id)
-    hash = {src: "test", inputPropertyId: property_id}
-    build_url do |params|
-      link_to( image_tag("rcma.png") + " RCMA Report" , "#{base_link_url}flow/rental-cma.html?#{hash.merge(params).to_query}", { target: "_blank"})
+    hash = "src=test&inputPropertyId=#{property_id}"
+    build_url(hash) do |params|
+      link_to( image_tag("rcma.png") + " RCMA Report" , "#{base_link_url}flow/rental-cma.html?#{params}", { target: "_blank"})
     end
   end
 
   def avm_link(property_id)
-    hash = {src: "test", inputPropertyId: property_id}
-    build_url do |params|
-      link_to( image_tag("avm.png") + " AVM Report" , "#{base_link_url}flow/avm.html?#{hash.merge(params).to_query}", { target: "_blank"})
+    hash = "src=test&inputPropertyId=#{property_id}"
+    build_url(hash) do |params|
+      link_to( image_tag("avm.png") + " AVM Report", "#{base_link_url}flow/avm.html?#{params}", { target: "_blank"})
     end
   end
 
   def rpp_property_link(property_id)
-    hash = { propertyId: property_id, is_workflow: 'false' , showNavBtns: 'false' }
-    build_url do |params|
-      link_to( image_tag('IconRPPro.png') + " Rpp property detail", "#{base_link_url}property/detail.html?#{hash.merge(params).to_query}".encode('utf-8'), {target: '_blank'} )
+    hash = "propertyId=#{property_id}&is_workflow=false&showNavBtns=false"
+    build_url(hash) do |params|
+      link_to( image_tag('IconRPPro.png') + " Rpp property detail", "#{base_link_url}property/detail.html?#{params}", {target: '_blank'} )
     end   
   end
 
   private
 
   def base_link_url
-    "https://rppstage.rpdata.com/rpp/"
+    "https://rpp.rpdata.com/rpp/"
   end
 
-  def build_url 
+  def build_url(query_string) 
     key = session[:api_key]
-    part1 = { timestamp: timestamp, token: session[:rp_data_token], apiKey: key }
-    hash_part = { hash: OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), key, part1.to_query).to_s } 
-    yield part1.merge(hash_part)
+    part1_url = "#{query_string}&timestamp=#{timestamp}&token=#{session[:rp_data_token]}&apiKey=#{key}"
+    hash_part = "hash=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), 'fa002dea8e26c362e20c21ef0290b3deee3db674', part1_url).to_s}"
+    yield "#{part1_url}&#{hash_part}"
   end
 
   def timestamp 
